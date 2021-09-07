c5d222d796cb2b4a23efa0bf656202d4b87698da
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-09-07 15:03:53 -0300

    Projetos de backend e frontend criados

---
+++ backend/src/main/java/br/com/bts/cbvendas/CbvendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.bts.cbvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/bts/cbvendas/CbvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bts.cbvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
